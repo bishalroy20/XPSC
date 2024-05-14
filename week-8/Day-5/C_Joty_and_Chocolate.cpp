e1cb2cb67f6bab8b1bbe40f80694bbca83284b1c
bishalroy20 <163531692+bishalroy20@users.noreply.github.com>
2024-05-14 23:15:55 +0600

    Add files via upload
    added week8 day5

---
+++ week-8/Day-5/C_Joty_and_Chocolate.cpp
@@ -0,0 +1,50 @@
+/*
+    author : BishalRoy20
+*/
+#include<bits/stdc++.h>
+#define int long long int
+#define pii pair<ll,ll>
+#define yes cout << "YES" << '\n'
+#define no cout << "NO" << '\n'
+#define cyes cout << "Yes" << '\n'
+#define cno cout << "No" << '\n'
+#define minus cout << -1 << '\n'
+#define fastread() (ios_base::sync_with_stdio(0),cin.tie(0));
+using namespace std;
+const int mod = 1e9 + 7;
+
+long long gcd(long long a, long long b) {
+    return __gcd(a, b);
+}
+
+long long lcm(long long a, long long b) {
+    return ((a / gcd(a, b)) * b);
+}
+
+void solve(){
+    long long n, a, b, p, q, ans = 0, overlap;
+    cin >> n >> a >> b >> p >> q;
+
+    ans += ((n / a) * p);
+    ans += ((n / b) * q);
+    overlap = (n / lcm(a, b));
+    ans -= (overlap * (p + q));
+    ans += (overlap * max(p, q));
+
+    cout << ans << '\n';
+}
+
+int32_t main()
+{
+    fastread();
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+
